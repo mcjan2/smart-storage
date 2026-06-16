0509239eb77c1e5902cb42a739bc2249bd61210e
Ketchup_HD <vketchuphdv@gmail.com>
2026-06-16 12:32:39 +0200

    test commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1 className="big-heading">Test</h1>
+      <h1 className="big-heading">Du Rentner</h1>
     </div>
   );
 }
