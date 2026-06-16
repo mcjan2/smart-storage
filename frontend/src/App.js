d5fc5b487c04f5c17baa1fb47a84ad39337df283
mcjan2 <76522000+mcjan2@users.noreply.github.com>
2026-06-16 11:49:30 +0200

    App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1 className="big-heading">Test</h1>
+      <h1 className="big-heading">Test32</h1>
     </div>
   );
 }
